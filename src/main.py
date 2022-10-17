0e7a1ff3b72a01419322c7b4aa36d84314a31263
aerits <stev_nm@protonmail.com>
2022-10-16 19:38:23 -0600

    src/main.py

--- src/main.py
+++ src/main.py
@@ -8,20 +8,6 @@ from textual.widget import Widget
 
 from Sidebar import Bar
 
-class Hover(Widget):
-
-    mouse_over = Reactive(False)
-
-    def render(self) -> Panel:
-        return Panel("Hello World")
-
-    def on_enter(self) -> None:
-        self.mouse_over = True
-
-    def on_leave(self) -> None:
-        self.mouse_over = False
-
-
 class main(App):
     """Demonstrates custom widgets"""
 
