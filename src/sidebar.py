4c225cfa2b8f3c4538dd4d0a29b3d01fa87520bf
aerits <stev_nm@protonmail.com>
2022-10-16 19:28:52 -0600

    src/sidebar.py

---
+++ src/sidebar.py
@@ -0,0 +1,12 @@
+class Hover(Widget):
+
+    mouse_over = Reactive(False)
+
+    def render(self) -> Panel:
+        return Panel("Hello World")
+
+    def on_enter(self) -> None:
+        self.mouse_over = True
+
+    def on_leave(self) -> None:
+        self.mouse_over = False
