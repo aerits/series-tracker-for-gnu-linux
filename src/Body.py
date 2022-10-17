8ff36e666a0f80977ea0b1c8023a554bc93fa598
aerits <stev_nm@protonmail.com>
2022-10-16 19:39:35 -0600

    src/Body.py

--- src/Body.py
+++ src/Body.py
@@ -4,7 +4,7 @@ from textual.app import App
 from textual.reactive import Reactive
 from textual.widget import Widget
 
-class Body(Widget):
+class body(Widget):
 
     mouse_over = Reactive(False)
 
