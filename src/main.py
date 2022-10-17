9f2d5b465a8ccaf6a61f27b4a07d221dbd28864c
aerits <stev_nm@protonmail.com>
2022-10-16 18:47:53 -0600

    src/main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,21 @@
+from rich.panel import Panel
+
 from textual.app import App
-from textual.widgets import Placeholder
+from textual.reactive import Reactive
+from textual.widget import Widget
+
+class Hover(Widget):
+
+    mouse_over = Reactive(False)
+
+    def render(self) -> Panel:
+        return Panel("Hello [b]World[/b]", style=("on red" if self.mouse_over else ""))
+
+    def on_enter(self) -> None:
+        self.mouse_over = True
+
+    def on_leave(self) -> None:
+        self.mouse_over = False
 
 
 class SimpleApp(App):
