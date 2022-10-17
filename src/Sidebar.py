556a855244785eee2803b24bbc14bbd243406848
aerits <stev_nm@protonmail.com>
2022-10-16 19:37:15 -0600

    src/Sidebar.py

--- src/Sidebar.py
+++ src/Sidebar.py
@@ -9,7 +9,7 @@ class Bar(Widget):
     mouse_over = Reactive(False)
 
     def render(self) -> Panel:
-        return Panel("Hello World")
+        return Panel("sidebar")
 
     def on_enter(self) -> None:
         self.mouse_over = True
